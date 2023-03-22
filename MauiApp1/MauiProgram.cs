55cbdf1167481fc677cd1cd035a4cea7473bc2ea
MikeL Kamel <mikel1832@yahoo.com>
2023-03-21 20:39:13 -0400

    Initial commit

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿#if IOS || MACCATALYST
+global using Foundation;
+global using ObjCRuntime;
+global using WebKit;
+#endif
+
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using MauiApp1.Data;
+
+
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		builder.Services.AddMauiBlazorWebView();
+		#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
